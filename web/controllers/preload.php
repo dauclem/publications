3bc33e82e24207d857f2facdad3df7e32f3aae63
dauclem <dauclem@gmail.com>
2013-09-18 13:44:15 +0200

    fix preload

--- web/controllers/preload.php
+++ web/controllers/preload.php
@@ -9,6 +9,6 @@ $base_url = $config_shared->get_site_url().'all.php?project_id=';
 
 foreach ($project_shared->get_projects() as $project) {
 	if ($project->is_visible()) {
-		file($base_url.$project->get_id());
+		file($project->get_url_see_all());
 	}
 }

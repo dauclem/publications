d2a01d7a787823827ee80ad6f8ab437618db1c21
dauclem <dauclem@gmail.com>
2014-01-28 17:12:16 +0100

    #21 Transform temporary publication to real on auto_publi call (to keep comment)

--- web/controllers/auto_publi.php
+++ web/controllers/auto_publi.php
@@ -3,9 +3,14 @@
 require __DIR__.'/../../include/setup.php';
 
 /** @var \Interfaces\Shared\Project $project_shared */
-$project_shared = $dic->getObject('project');
+$project_shared  = $dic->getObject('project');
 $current_project = $project_shared->getCurrentProject();
 
 /** @var \Interfaces\Shared\Publication $publication_shared */
 $publication_shared = $dic->getObject('publication');
-$publication_shared->create($current_project, false, time(), '');
+$publication_temp   = $publication_shared->getPublicationTemp($current_project);
+if ($publication_temp) {
+	$publication_temp->setTemp(false);
+} else {
+	$publication_shared->create($current_project, false, time(), '');
+}

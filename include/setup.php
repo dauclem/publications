6296f6a7be2ddda354ec82cc9340d01318e4ee1e
dauclem <cedric.roustan@gmail.com>
2015-12-08 09:52:39 +0100

    no memory limit

--- include/setup.php
+++ include/setup.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 set_time_limit(0);
 ini_set('max_execution_time', 0);
+ini_set('memory_limit', -1);
 session_start();
 
 $base_dir = dirname(__DIR__);
